f7592b17296c7617ec1d78ecfc30e0a4b9884976
Flavio Galon <flaviogalon@gmail.com>
2024-04-11 19:38:47 +0100

    Add shortcut to search for methods/functions in a file

--- nvim/lua/galon/plugins/telescope.lua
+++ nvim/lua/galon/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
     telescope.setup({
       defaults = {
@@ -32,5 +33,11 @@ return {
     keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set(
+      "n",
+      "<leader>fm",
+      "<cmd>lua require('telescope.builtin').lsp_document_symbols({ symbols = { 'function', 'class', 'method' }, symbol_width = 50 })<cr>",
+      { desc = "Find methods/functions in current file" }
+    )
   end,
 }
